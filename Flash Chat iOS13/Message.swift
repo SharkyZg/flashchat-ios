4af89e3e0cb5e9f803bf99fadfabaafdc1fa8331
Marko Sarkanj <marko.sarkanj@gmail.com>
2021-02-24 14:02:26 +0100

    project complete

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Marko Sarkanj on 23.02.21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
